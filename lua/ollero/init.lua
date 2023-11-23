800ea2357208e51c91a8ea626d90279ceb37f61e
Marco Antônio <ma.souza.junior@gmail.com>
2023-11-23 12:47:56 -0300

    fea: add mappings and commands

--- lua/ollero/init.lua
+++ lua/ollero/init.lua
@@ -9,11 +9,46 @@ local WIN_W = 120
 
 local M = {}
 
+local apply_commands = function(mappings)
+  for k, v in pairs(mappings) do
+    local command = k
+    local handler = v[1]
+
+    vim.api.nvim_create_user_command(command, handler, {})
+  end
+end
+
+local apply_mappings = function(mappings)
+  for k, v in pairs(mappings) do
+    local mapping = k
+    local handler = v[1]
+
+    vim.keymap.set(
+      { "n", "v", "i", "t" },
+      mapping,
+      handler,
+      { noremap = true, silent = true }
+    )
+  end
+end
+
 function M.setup()
   -- setup
   terminal.toggle("vertical")
   terminal.send("ollama run llama2", "vertical")
   terminal.toggle("vertical")
+
+  -- create commands
+  apply_commands({
+    ["Chat"] = { M.openTerm },
+    ["Ask"] = { M.open },
+  })
+
+  -- add keymaps
+  apply_mappings({
+    ["<leader>aa"] = { M.openTerm },
+    ["<leader>ac"] = { M.open },
+  })
 end
 
 function M.open()
@@ -22,7 +57,7 @@ function M.open()
   win.input(opts, function(input)
     local close_loading = win.loading_llama(opts)
 
-    require("ollero.ollama").ask_llama(input, function(output)
+    require("ollero.ollama").ask_ollama(input, function(output)
       close_loading()
 
       win.output(
