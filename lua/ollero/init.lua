479fc19d4d012f6eb0372ad287795ad9ae5f7d3f
Marco Antônio <ma.souza.junior@gmail.com>
2023-11-25 18:18:53 -0300

    enh: fix module setup

--- lua/ollero/init.lua
+++ lua/ollero/init.lua
@@ -1,4 +1,8 @@
 Name = "Ollero"
 Description = "A plugin to open an input box for chating with Ollama"
 
-require("ollero.ollero").init()
+return {
+  setup = function()
+    require("ollero.ollero").init()
+  end,
+}
