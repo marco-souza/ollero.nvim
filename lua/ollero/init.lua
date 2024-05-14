a1a3e02d5b7caeea5844ce353e434100081659bc
Marco Antônio <ma.souza.junior@gmail.com>
2024-05-14 18:48:22 -0300

    enh: improve init typing

--- lua/ollero/init.lua
+++ lua/ollero/init.lua
@@ -1,8 +1,14 @@
 Name = "Ollero"
 Description = "A plugin to open an input box for chating with Ollama"
 
-return {
+---@class OlleroOptions
+---@field model 'llama2' | 'llama3'
+
+local M = {
+  ---@param opts OlleroOptions
   setup = function(opts)
     require("ollero.ollero").init(opts)
   end,
 }
+
+return M
