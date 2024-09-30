80eb6dd7f11697e39e51efce5a73fa8fece641a3
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-30 11:14:20 -0300

    oth: remove debug log

--- lua/shared/tests.lua
+++ lua/shared/tests.lua
@@ -2,7 +2,6 @@ local M = {}
 
 -- setup resolution path
 package.path = package.path .. ";./lua/?.lua"
-vim.print({ package = package, path = package.path })
 
 local log = require("shared.logger").create_logger({ log_level = "debug" })
 
